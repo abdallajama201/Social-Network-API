88a4d2b1cd66b5244effaed1de67c2fac6d493ac
Abdalla Jama <abdallajama201@gmail.com>
2022-07-13 09:55:30 -0400

    adjusting models

--- models/user.js
+++ models/user.js
@@ -6,11 +6,18 @@ const validator = (val) => {
 }
 
 const userSchema = new mongoose.Schema({
-   username: {type: String, unique: true, required: true, trim: true},
-   email: {type: String, unique: true, required: true, validate: validator},
-   thoughts: [{type: mongoose.Schema.Types.ObjectId, ref: "Thought"}],
-   friends: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}]
-});
+        username: {type: String, unique: true, required: true, trim: true},
+        email: {type: String, unique: true, required: true, validate: validator},
+        thoughts: [{type: mongoose.Schema.Types.ObjectId, ref: "Thought"}],
+        friends: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
+);
 
 userSchema.virtual("friendCount").get(function() {
     return this.friends.length;
